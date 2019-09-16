22895457018cea642b179cecaa22bc00ca4db91e
Carlos Eduardo Rodrigues de Sousa <carloseduardordesousa@gmail.com>
2019-09-16 00:49:57 -0300

    feat: Armazenamento em lista dinamica adicionado

--- trabalho.cpp
+++ trabalho.cpp
@@ -7,6 +7,14 @@
     //decodificar a frase
 #include <stdio.h>
 #include <string.h>
+#include <malloc.h>
+typedef struct estrutura{
+    char chave;
+    struct estrutura *prox;
+} NO;
+typedef struct{
+    NO* inicio;
+} LISTA;
 bool isNumber(char caractere){
     switch((int)caractere){
         case 49:
@@ -33,24 +41,71 @@ bool isNumber(char caractere){
         return false;
     }
 }
-void decodificar (char frase[]){
-    char fraseDecod[100];
+int tamanho(LISTA* l){
+    int tam = 0;
+    NO* end = l->inicio;
+    while(end != NULL){
+        tam++;
+        end = end->prox;
+    }
+    return tam;
+}
+void exibeLista(LISTA l){
+    NO* end = l.inicio;
+    printf("Lista: \" ");
+    while(end){
+        printf("%c", end->chave);
+        end = end ->prox;
+    }
+    printf("\"\n");
+}
+NO* ultimoElementoLista(LISTA* l) {
+    NO* elemento = l->inicio;
+    if(elemento){
+        while(elemento->prox){
+            elemento = elemento->prox;
+        }
+    }
+    return(elemento);
+}
+void insereElemento(LISTA* l, char ch){
+    NO* novo;
+    NO* ant;
+    ant = ultimoElementoLista(l);
+    novo = (NO*) malloc(sizeof(NO));
+    novo->chave = ch;
+    novo->prox = NULL;
+    if(!ant){
+        l->inicio = novo;
+    }else{
+        ant->prox = novo;
+    }
+}
+NO* decodificar (char* frase, LISTA* l){
     for(int i=0;i<strlen(frase);i++){
         if(isNumber(frase[i])){
             if(i==strlen(frase)-1){
-                printf("%c",frase[i]);
+                insereElemento(l,frase[i]);
             }
             int j = frase[i] - '0';
             for(int k=0;k<j;k++){
-                printf("%c",frase[i+1]);
+                insereElemento(l,frase[i+1]);
             }
             i++;
         }else{
-            printf("%c",frase[i]);
+            insereElemento(l,frase[i]);
         }
     }
+    return l->inicio;
+}
+void inicializarLista(LISTA* l){
+    l->inicio = NULL;
 }
 int main(){
+    LISTA l;
+    LISTA* pont;
+    pont = &l;
+    inicializarLista(pont);
     char frase[100] = "Li 12 vezes mas não entendi n4ada5! Vou tirar 0010.";
-    decodificar(frase);
+    decodificar(frase,pont);
 }
