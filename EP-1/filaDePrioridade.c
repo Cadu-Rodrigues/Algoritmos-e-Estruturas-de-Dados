8b38a5665adcc003fe9d664608016cb50eb29d9d
Carlos Eduardo <carloseduardordesousa@gmail.com>
2020-10-06 00:03:24 -0300

    [Feature]: Adiciona funcao de retorno do tamanho da fila

--- EP-1/filaDePrioridade.c
+++ EP-1/filaDePrioridade.c
@@ -5,19 +5,21 @@
 /**                                                                 **/
 /**   EP1 - Fila de Prioridade                                      **/
 /**                                                                 **/
-/**   <nome do(a) aluno(a)>                   <numero USP>          **/
+/**   Carlos Eduardo Rodrigues de Sousa                   11295758  **/
 /**                                                                 **/
 /*********************************************************************/
 
 #include "filaDePrioridade.h"
 
-PFILA criarFila(int max){
-  PFILA res = (PFILA) malloc(sizeof(FILADEPRIORIDADE));
+PFILA criarFila(int max)
+{
+  PFILA res = (PFILA)malloc(sizeof(FILADEPRIORIDADE));
   res->maxElementos = max;
-  res->arranjo = (PONT*) malloc(sizeof(PONT)*max);
+  res->arranjo = (PONT *)malloc(sizeof(PONT) * max);
   int i;
-  for (i=0;i<max;i++) res->arranjo[i] = NULL;
-  PONT cabeca = (PONT) malloc(sizeof(ELEMENTO));
+  for (i = 0; i < max; i++)
+    res->arranjo[i] = NULL;
+  PONT cabeca = (PONT)malloc(sizeof(ELEMENTO));
   res->fila = cabeca;
   cabeca->ant = cabeca;
   cabeca->prox = cabeca;
@@ -26,41 +28,47 @@ PFILA criarFila(int max){
   return res;
 }
 
-bool exibirLog(PFILA f){
+bool exibirLog(PFILA f)
+{
   printf("Log [elementos: %i (alem do no cabeca)]\n", tamanho(f));
   PONT atual = f->fila;
   printf("%p[%i;%f;%p]%p ", atual->ant, atual->id, atual->prioridade, atual, atual->prox);
   atual = atual->prox;
-  while (atual != f->fila){
+  while (atual != f->fila)
+  {
     printf("%p[%i;%f;%p]%p ", atual->ant, atual->id, atual->prioridade, atual, atual->prox);
     atual = atual->prox;
   }
   printf("\nElementos validos: ");
   atual = atual->prox;
-  while (atual != f->fila){
+  while (atual != f->fila)
+  {
     printf("[%i;%f;%p] ", atual->id, atual->prioridade, atual);
     atual = atual->prox;
   }
 
-  printf("\nValores do arrajo:\n\[ ");
+  printf("\nValores do arranjo:\n\[ ");
   int x;
-  for (x=0;x<f->maxElementos;x++) printf("%p ",f->arranjo[x]);
+  for (x = 0; x < f->maxElementos; x++)
+    printf("%p ", f->arranjo[x]);
   printf("]\n\n");
 }
 
+int tamanho(PFILA f)
+{
 
-
-int tamanho(PFILA f){
+  PONT iterador = f->fila->prox;
   int tam = 0;
-
-  /* COMPLETAR */
-
+  while (iterador != f->fila)
+  {
+    tam++;
+    iterador = iterador->prox;
+  }
   return tam;
 }
 
-
-
-bool inserirElemento(PFILA f, int id, float prioridade){
+bool inserirElemento(PFILA f, int id, float prioridade)
+{
   bool resposta = false;
 
   /* COMPLETAR */
@@ -68,9 +76,8 @@ bool inserirElemento(PFILA f, int id, float prioridade){
   return resposta;
 }
 
-
-
-bool aumentarPrioridade(PFILA f, int id, float novaPrioridade){
+bool aumentarPrioridade(PFILA f, int id, float novaPrioridade)
+{
   bool resposta = false;
 
   /* COMPLETAR */
@@ -78,9 +85,8 @@ bool aumentarPrioridade(PFILA f, int id, float novaPrioridade){
   return resposta;
 }
 
-
-
-bool reduzirPrioridade(PFILA f, int id, float novaPrioridade){
+bool reduzirPrioridade(PFILA f, int id, float novaPrioridade)
+{
   bool resposta = false;
 
   /* COMPLETAR */
@@ -88,9 +94,8 @@ bool reduzirPrioridade(PFILA f, int id, float novaPrioridade){
   return resposta;
 }
 
-
-
-PONT removerElemento(PFILA f){
+PONT removerElemento(PFILA f)
+{
   PONT resposta = NULL;
 
   /* COMPLETAR */
@@ -98,14 +103,11 @@ PONT removerElemento(PFILA f){
   return resposta;
 }
 
-
-
-bool consultarPrioridade(PFILA f, int id, float* resposta){
+bool consultarPrioridade(PFILA f, int id, float *resposta)
+{
   bool resp = false;
 
   /* COMPLETAR */
 
   return resp;
 }
-
-
