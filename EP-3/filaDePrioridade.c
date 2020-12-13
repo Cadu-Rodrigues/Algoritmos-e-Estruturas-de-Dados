681e4931e26bb8de1934a23f3f458bfbb76f11a5
Carlos Eduardo <carloseduardordesousa@gmail.com>
2020-12-13 06:04:52 -0300

    [Feature]: Insercao de elementos adicionada

--- EP-3/filaDePrioridade.c
+++ EP-3/filaDePrioridade.c
@@ -39,7 +39,51 @@ void exibirLog(PFILA f)
   }
   printf("\n\n");
 }
+bool elementoInseridoEm(FILADEPRIORIDADE fila, int id)
+{
+  int i;
+  for (i = 0; i < fila.elementosNoHeap; i++)
+  {
+    if (id == fila.heap[i]->id)
+      return true;
+  }
+  return false;
+}
+void ajeitaHeap(PFILA A, int m, int i)
+{
+  int esq;
+  int dir;
+  if (i == 0)
+  {
+    esq = 1;
+    dir = 2;
+  }
+  else
+  {
+    esq = 2 * i;
+    dir = (2 * i) + 1;
+  }
 
+  int maior;
+  if (A->heap[esq] && esq < m && A->heap[esq]->prioridade > A->heap[i]->prioridade)
+    maior = esq;
+  else
+    maior = i;
+  if (A->heap[dir] && dir < m && A->heap[dir]->prioridade > A->heap[maior]->prioridade)
+    maior = dir;
+  if (maior != i)
+  {
+    ELEMENTO *temp = (PONT)malloc(sizeof(ELEMENTO));
+    int posI = A->heap[i]->posicao;
+    int posM = A->heap[maior]->posicao;
+    temp = A->heap[i];
+    A->heap[i] = A->heap[maior];
+    A->heap[maior] = temp;
+    A->heap[i]->posicao = posI;
+    A->heap[maior]->posicao = posM;
+    ajeitaHeap(A, m, maior);
+  }
+}
 int tamanho(PFILA f)
 {
   int tam = 0;
@@ -51,11 +95,19 @@ int tamanho(PFILA f)
 
 bool inserirElemento(PFILA f, int id, float prioridade)
 {
-  bool res = false;
-
-  /* COMPLETAR */
-
-  return res;
+  if (id < 0 || id >= f->maxElementos)
+    return false;
+  if (elementoInseridoEm(*f, id))
+    return false;
+  f->elementosNoHeap++;
+  PONT novo = (PONT)malloc(sizeof(ELEMENTO));
+  novo->id = id;
+  novo->prioridade = prioridade;
+  novo->posicao = f->elementosNoHeap - 1;
+  f->arranjo[id] = &*novo;
+  f->heap[f->elementosNoHeap - 1] = &*novo;
+  ajeitaHeap(f, f->elementosNoHeap, 0);
+  return true;
 }
 
 bool aumentarPrioridade(PFILA f, int id, float novaPrioridade)
